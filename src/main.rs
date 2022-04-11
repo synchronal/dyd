2f0bfdf5c94fed3749b96803f421486ab77fe13f
Eric Saxby <sax@livinginthepast.org>
2022-04-11 10:32:20 -0700

    Add tui/crossterm

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use dyd::manifest::Manifest;
 
 use anyhow::{Context, Result};
 use clap::Parser;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::thread;
+use std::time::Duration;
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Block, Borders};
+use tui::Terminal;
 
 #[derive(Parser)]
 struct Cli {
@@ -10,12 +16,31 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
+    let _manifest = parse_manifest();
+
+    enable_raw_mode()?;
+
+    let stdout = std::io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Diff").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    disable_raw_mode()?;
+
+    Ok(())
+}
+
+fn parse_manifest() -> Result<Manifest, Box<dyn std::error::Error> {
+let args = Cli::parse();
     let manifest_contents = std::fs::read_to_string(&args.manifest)
         .with_context(|| format!("Error reading file: `{}`", &args.manifest.to_str().unwrap()))?;
 
-    let manifest: Manifest = toml::from_str(&manifest_contents)?;
-
-    println!("manifest: {:?}", manifest);
-    Ok(())
+    toml::from_str(&manifest_contents)?
 }
