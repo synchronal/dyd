ae2f5a7b775976197a8808cbeb75a72984428f3b
Eric Saxby <sax@livinginthepast.org>
2022-08-13 12:09:48 -0700

    Split time functions into separate files

---
+++ src/time/unix.rs
@@ -0,0 +1,36 @@
+use crate::app::AppResult;
+use chrono::prelude::*;
+
+/// Transforms a UNIX timestamp to a DateTime in UTC.
+pub fn parse_unix(time: &str) -> AppResult<DateTime<Utc>> {
+    let timestamp = time.parse::<i64>()?;
+    let naive = NaiveDateTime::from_timestamp(timestamp, 0);
+    let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+
+    Ok(datetime)
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::prelude::*;
+    use std::error::Error;
+    use std::num::ParseIntError;
+
+    #[test]
+    fn parse_unix_ok() -> Result<(), Box<dyn Error>> {
+        let time = super::parse_unix(&"1650989481".to_owned())?;
+
+        let naive = NaiveDate::from_ymd(2022, 4, 26).and_hms(16, 11, 21);
+        let expected: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+
+        assert_eq!(time, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn parse_unix_error() {
+        let error = super::parse_unix(&"hello".to_owned()).unwrap_err();
+        assert!(error.is::<ParseIntError>());
+    }
+}
