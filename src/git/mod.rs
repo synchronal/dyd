f06817817910d7f290ed39c368f9b34282448fcd
Eric Saxby <sax@livinginthepast.org>
2023-12-30 10:36:49 -0800

    Add logging to git operations

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,5 +1,6 @@
 use crate::app::AppResult;
 use gix::remote::Direction;
+use log;
 use std::error::Error;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -10,16 +11,19 @@ pub mod repo;
 static GIT_FORMAT: &str = "%h\x0B%ct\x0B%ch\x0B%an\x0B%s";
 
 pub fn clone_repo(origin: String, path: &Path) -> Result<(), Box<dyn Error>> {
+  log::info!("starting git clone: remote: \"{origin}\", path: {:?}", path);
   std::fs::create_dir_all(path)?;
-  let mut prepare_clone = gix::prepare_clone(origin, path)?;
+  let mut prepare_clone = gix::prepare_clone(origin.clone(), path)?;
 
   let (mut prepare_checkout, _) =
     prepare_clone.fetch_then_checkout(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
 
   let (repo, _) = prepare_checkout.main_worktree(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
   let _remote = repo
-    .find_default_remote(gix::remote::Direction::Fetch)
+    .find_default_remote(Direction::Fetch)
     .expect("always present after clone")?;
+
+  log::info!("finished git clone: remote: \"{origin}\", path: {:?}", path);
 
   Ok(())
 }
@@ -46,22 +50,34 @@ pub fn logs(path: &PathBuf, branch: &Option<String>) -> AppResult<Vec<u8>> {
 }
 
 pub fn pull_repo(path: &PathBuf) -> AppResult<()> {
+  log::info!("starting git fetch: {:?}", path);
   let repo = gix::discover(path)?;
-  let remote = repo
-    .head()?
-    .into_remote(Direction::Fetch)
-    .expect("present")?;
+  log::debug!("repo: {:?}", repo);
+  let head = repo.head()?;
+  let remote = match head.into_remote(Direction::Fetch) {
+    Some(r) => r?,
+    None => {
+      log::error!("failed git fetch: {:?}, reason: \"unable to find remote\"", path);
+      log::debug!("failed repo: {:?}", repo);
+      return Err("Unable to fetch remote".into());
+    }
+  };
 
   remote
     .connect(Direction::Fetch)?
     .prepare_fetch(gix::progress::Discard, Default::default())?
     .receive(gix::progress::Discard, &AtomicBool::default())?;
 
+  log::info!("finished git fetch: {:?}", path);
+  log::info!("starting git merge: {:?}", path);
+
   Command::new("git")
     .args(["merge", "--no-edit", "--ff-only", "--quiet", "--no-commit"])
     .current_dir(path)
     .output()
     .unwrap();
+
+  log::info!("finished git merge: {:?}", path);
 
   Ok(())
 }
