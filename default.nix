82c226e9a7f38fad804af6a801cea22881aef30e
Eric Saxby <sax@livinginthepast.org>
2024-01-22 16:06:38 -0800

    Update nix flake to 1.8.0
    Co-authored-by: Erik Hanson <erik.h@maersk.com>

--- default.nix
+++ default.nix
@@ -1,6 +1,21 @@
-{ rustPlatform }:
+{ lib, fetchFromGitHub, rustPlatform }:
 rustPlatform.buildRustPackage {
-  name = "dyd";
-  src = ./.;
-  cargoHash = "sha256-OMnw8AkdeiG9SGDgCiaDbPBcor3+E696Z171M/7rApM=";
+  pname = "dyd";
+  version = "1.8.0";
+
+  src = fetchFromGitHub {
+    owner = "synchronal";
+    repo = "dyd";
+    rev = "v1.8.0";
+    hash = "sha256-DmdrOacLOxb8v5niYmSCmxvU3bcLzRlZa+JROn4HDHE=";
+  };
+
+  cargoHash = "sha256-45gqEanUjzXNJz/muWps/HUJqKLx8/YCh18L4gIJhSk=";
+
+  meta = with lib; {
+    description = "Daily diff";
+    homepage = "https://github.com/synchronal/dyd";
+    license = licenses.mit;
+    maintainers = ["synchronal.dev"];
+  };
 }
