eb7743588c65fea8f4f17f9dda790baa728da632
Eric Saxby <sax@livinginthepast.org>
2025-06-17 14:08:41 -0700

    Move core functionality to lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,16 @@
 //!
 //! It relies on the shell for authentication to remote origins, and uses
 //! the configured GUI git difftool for opening diffs.
+
+use crate::app::handler::handle_key_events;
+use crate::app::{App, AppResult, Event, EventHandler};
+use crate::manifest::Manifest;
+use crate::terminal::Tui;
+use ratatui::backend::CrosstermBackend;
+use ratatui::Terminal;
+use std::io::Write;
+use std::path::PathBuf;
+
 pub mod app;
 pub mod cli;
 pub mod config;
@@ -14,3 +24,70 @@ pub mod terminal;
 pub mod time;
 pub mod ui;
 pub mod widget;
+
+pub fn clean(share_path: PathBuf, verbose: bool) -> AppResult<()> {
+  if share_path.exists() {
+    if verbose {
+      for repo in share_path
+        .read_dir()
+        .expect("Expected .local/share/dyd to be a directory")
+        .flatten()
+      {
+        println!("clean: {:?}", repo.path());
+        log::info!("clean: {:?}", repo.path());
+        std::fs::remove_dir_all(repo.path())?;
+      }
+    }
+    log::info!("clean: {:?}", share_path);
+    std::fs::remove_dir_all(share_path)?;
+  }
+  Ok(())
+}
+
+pub fn diff(manifest: PathBuf, share_path: PathBuf) -> AppResult<()> {
+  let manifest = Manifest::new(manifest, share_path)?;
+  let mut app: App = manifest.into();
+
+  let backend = CrosstermBackend::new(std::io::stderr());
+  let terminal = Terminal::new(backend)?;
+  let events = EventHandler::new(250);
+  let mut tui = Tui::new(terminal, events);
+
+  tui.init()?;
+
+  while app.running {
+    tui.draw(&mut app)?;
+
+    match tui.events.next()? {
+      Event::Tick(sender) => app.tick(sender)?,
+      Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+      Event::Mouse(_) => {}
+      Event::Resize(_, _) => {}
+      Event::RepoStatusChange(id, state) => app.update_repo_status(id, state)?,
+      Event::RepoStatusComplete(id, logs) => app.update_repo_logs(id, logs)?,
+    }
+  }
+
+  tui.exit()?;
+
+  Ok(())
+}
+
+pub fn write_default_manifest(manifest_path: PathBuf) -> AppResult<()> {
+  println!("Creating file: {:?}", &manifest_path);
+
+  let mut file = std::fs::OpenOptions::new()
+    .create_new(true)
+    .append(true)
+    .open(manifest_path)?;
+
+  writeln!(file, "since = \"5 days ago\"")?;
+  writeln!(file)?;
+  writeln!(file, "[remotes]")?;
+  writeln!(file)?;
+  writeln!(file, "[remotes.dyd]")?;
+  writeln!(file, "name = \"Daily diff\"")?;
+  writeln!(file, "origin = \"git@github.com:sychronal/dyd\"")?;
+  writeln!(file)?;
+  Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use dyd::app::handler::handle_key_events;
-use dyd::app::{App, AppResult};
-use dyd::app::{Event, EventHandler};
+use dyd::app::AppResult;
 use dyd::cli::{Command, CLI};
 use dyd::config::Config;
-use dyd::manifest::Manifest;
-use dyd::terminal::Tui;
 
 use anyhow::Context;
 
@@ -13,9 +9,6 @@ use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
 use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
 use log4rs::append::rolling_file::RollingFileAppender;
 use log4rs::encode::pattern::PatternEncoder;
-use ratatui::backend::CrosstermBackend;
-use ratatui::Terminal;
-use std::io::Write;
 use std::path::{Path, PathBuf};
 
 fn main() -> AppResult<()> {
@@ -30,80 +23,10 @@ fn main() -> AppResult<()> {
   let _config = Config::load();
 
   match command {
-    Command::Clean { verbose } => clean(verbose),
-    Command::Diff(args) => diff(args.manifest, share_path),
-    Command::Init(args) => write_default_manifest(args.manifest),
+    Command::Clean { verbose } => dyd::clean(share_path, verbose),
+    Command::Diff(args) => dyd::diff(args.manifest, share_path),
+    Command::Init(args) => dyd::write_default_manifest(args.manifest),
   }
-}
-
-fn clean(verbose: bool) -> AppResult<()> {
-  let home = std::env::var("HOME").context("Unable to access HOME")?;
-  let path = Path::new(&home).join(".local/share/dyd");
-
-  if path.exists() {
-    if verbose {
-      for repo in path
-        .read_dir()
-        .expect("Expected .local/share/dyd to be a directory")
-        .flatten()
-      {
-        println!("clean: {:?}", repo.path());
-        log::info!("clean: {:?}", repo.path());
-        std::fs::remove_dir_all(repo.path())?;
-      }
-    }
-    log::info!("clean: {:?}", path);
-    std::fs::remove_dir_all(path)?;
-  }
-  Ok(())
-}
-
-fn diff(manifest: PathBuf, share_path: PathBuf) -> AppResult<()> {
-  let manifest = Manifest::new(manifest, share_path)?;
-  let mut app: App = manifest.into();
-
-  let backend = CrosstermBackend::new(std::io::stderr());
-  let terminal = Terminal::new(backend)?;
-  let events = EventHandler::new(250);
-  let mut tui = Tui::new(terminal, events);
-
-  tui.init()?;
-
-  while app.running {
-    tui.draw(&mut app)?;
-
-    match tui.events.next()? {
-      Event::Tick(sender) => app.tick(sender)?,
-      Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
-      Event::Mouse(_) => {}
-      Event::Resize(_, _) => {}
-      Event::RepoStatusChange(id, state) => app.update_repo_status(id, state)?,
-      Event::RepoStatusComplete(id, logs) => app.update_repo_logs(id, logs)?,
-    }
-  }
-
-  tui.exit()?;
-
-  Ok(())
-}
-
-fn write_default_manifest(manifest_path: PathBuf) -> AppResult<()> {
-  println!("Creating file: {:?}", &manifest_path);
-
-  let mut file = std::fs::OpenOptions::new()
-    .create_new(true)
-    .append(true)
-    .open(manifest_path)?;
-
-  writeln!(file, "since = \"5 days ago\"")?;
-  writeln!(file)?;
-  writeln!(file, "[remotes]")?;
-  writeln!(file)?;
-  writeln!(file, "[remotes.dyd]")?;
-  writeln!(file, "name = \"Daily diff\"")?;
-  writeln!(file, "origin = \"git@github.com:sychronal/dyd\"")?;
-  writeln!(file)?;
-  Ok(())
 }
 
 fn setup_dyd_config_path() -> AppResult<PathBuf> {
